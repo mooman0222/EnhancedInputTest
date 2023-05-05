cc0d0d061e341d99f9df84c7aa638f0b4a54d38d
MMN <mooman.che@gmail.com>
2023-05-06 02:46:39 +0900

    初期コミット

---
+++ README.md
@@ -0,0 +1,3 @@
+# EnhancedInputTest
+
+Developed with Unreal Engine 5
---
+++ Source/EnhancedInputTest/EnhancedInputTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EnhancedInputTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENHANCEDINPUTTEST_API AEnhancedInputTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
